caa05873e0de2cd201c9733c8446b6de29f28a9f
bathalasupraja <bathalasupraja2@gmail.com>
2024-05-24 10:49:43 +0530

    Initial commit
    Initial commit

---
+++ BookingApp/HotelTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HotelTableViewCell.swift
+//  BookingApp
+//
+//  Created by Supraja on 24/05/24.
+//
+
+import UIKit
+
+class HotelTableViewCell: UITableViewCell {
+    
+    static let id = "HotelTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
--- BookingApp/ViewController.swift
+++ BookingApp/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var hotelsTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        hotelsTableView.register(UINib(nibName: HotelTableViewCell.id, bundle: nil), forCellReuseIdentifier: HotelTableViewCell.id)
+        hotelsTableView.dataSource = self
+        hotelsTableView.delegate = self
+        
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: HotelTableViewCell.id, for: indexPath)
+        return cell
+    }
+}
